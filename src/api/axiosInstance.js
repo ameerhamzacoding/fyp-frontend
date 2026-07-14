357a5b842f970d2920353d5d16d9064c1c17af42
Ameerweb101 <ameerha1051@gmail.com>
2026-07-15 03:13:34 +0500

    Use env variable for API base URL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Points directly to your Express engine endpoints
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 // This automatically attaches your secure JWT session token to headers if a user is logged in
